f50b6e91456ae4c57789e11d5d3519bc15d6a356
Andrew Burks <andrewtburks@gmail.com>
2017-07-29 13:53:42 -0500

    Motor working, wired photoresistor

--- app.js
+++ app.js
@@ -8,7 +8,79 @@ var board = new five.Board({
 });
 
 board.on("ready", function() {
-  console.log("Device Ready..");
-  var led = new five.Led("D7");
-  led.blink();
+  console.log('ready');
+
+  let photoresistor = new five.Sensor({
+    pin: "A0",
+    freq: 250
+  });
+
+  // "data" get the current reading from the photoresistor
+  photoresistor.on("data", function() {
+    console.log(this.value)
+  });
+
+  var rightWheel = new five.Motor({
+    pins: { pwm: "D0", dir: "D4" },
+    pins: { pwm: "D0", dir: "D4" },
+    invertPWM: true
+  });
+
+  var leftWheel = new five.Motor({
+    pins: { pwm: "D1", dir: "D5" },
+    pins: { pwm: "D1", dir: "D5" },
+    invertPWM: true
+  });
+
+  var speed = 128;
+
+  function reverse() {
+    leftWheel.rev(speed);
+    rightWheel.rev(speed);
+  }
+
+  function forward() {
+    leftWheel.fwd(speed);
+    rightWheel.fwd(speed);
+  }
+
+  function stop() {
+    leftWheel.stop();
+    rightWheel.stop();
+  }
+
+  function left() {
+    leftWheel.rev(speed);
+    rightWheel.fwd(speed);
+  }
+
+  function right() {
+    leftWheel.fwd(speed);
+    rightWheel.rev(speed);
+  }
+
+  function exit() {
+    leftWheel.rev(0);
+    rightWheel.rev(0);
+    setTimeout(process.exit, 1000);
+  }
+
+  var keyMap = {
+    'up': forward,
+    'down': reverse,
+    'left': left,
+    'right': right,
+    'space': stop,
+    'q': exit
+  };
+
+  var stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.resume();
+
+  stdin.on("keypress", function(chunk, key) {
+      if (!key || !keyMap[key.name]) return;
+
+      keyMap[key.name]();
+  });
 });
